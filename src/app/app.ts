38fb745a67e061547263280c1073bf34c95d4864
Saulix187203 <saulo.ixcoy@gmail.com>
2026-02-09 01:05:28 -0600

    implementando angular material al componente login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './components/login/login';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,
+    Login,
+    FormsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login/login';
+import { LoginDto } from '../../interfaces/loginDto';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  private loginService = inject(LoginService);
 
+  username: string = '';
+  password: string = '';
+
+  login() {
+    console.log('Usuario:', this.username);
+    console.log('Password:', this.password);
+
+    if (this.username !== '' && this.password !== '') {
+      const objectRequest: LoginDto = {
+        username: this.username,
+        password: this.password 
+      };
+
+      this.loginService.doLogin(objectRequest).subscribe(entry => {
+        if (entry) {
+          console.log("login exitoso");
+        }
+      });
+    }
+
+  }
 }
